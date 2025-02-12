9f3d743ded0295a70229a101f82556df177ea43e
Humberto Fajardo <hafajardo@unicauca.edu.co>
2025-02-12 11:06:01 -0500

    Update InformeTarifas.py

--- InformeTarifas.py
+++ InformeTarifas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Carga y procesamiento de archivos")
+st.title("Informe tarifas")
 
 # Subir archivos
 uploaded_tc1 = st.file_uploader("Subir TC1.csv", type=["csv"])
@@ -23,10 +23,11 @@ if uploaded_tc1 and uploaded_tc2 and uploaded_ap and uploaded_divipola and uploa
     archivo_ap = extraer_datos_excel(uploaded_ap, "TABLA TARIFAS")
     davipola = pd.read_excel(uploaded_divipola)
     bitacora = pd.read_excel(uploaded_bitacora)
-    #Para validar que el archivo AP se leyo correctamente
+
+    # Validación de que el archivo AP se leyó correctamente
     st.write("### Columnas en AP:", archivo_ap.columns.tolist())
     
-    # Aplicar filtro por ID_COMERCIALIZADOR = 23442 fuera del bloque de carga
+    # Aplicar filtro por ID_COMERCIALIZADOR = 23442
     id_comercializador_col = 'ID COMERCIALIZADOR'  # Ajustar si es necesario
     niu_col = 'NIU'  # Ajustar si es necesario
     
@@ -36,3 +37,17 @@ if uploaded_tc1 and uploaded_tc2 and uploaded_ap and uploaded_divipola and uploa
         st.write(f"Número de NIUs en TC1 después de filtrar: {count_nius_tc1}")
     else:
         st.error("Las columnas esperadas no están en TC1. Verifica los nombres de las columnas.")
+
+    # Validación de TC2
+    if niu_col in tc2.columns:
+        tc2_sin_duplicados = tc2.drop_duplicates(subset=niu_col)
+        count_nius_tc2 = tc2_sin_duplicados[niu_col].nunique()
+        st.write(f"Número de NIUs en TC2 después de eliminar duplicados: {count_nius_tc2}")
+        
+        # Comparación entre TC1 y TC2
+        if count_nius_tc1 == count_nius_tc2 - 1:
+            st.success("El número de NIUs en TC2 coincide con el valor esperado.")
+        else:
+            st.error("El número de NIUs en TC2 no coincide con el valor esperado. Verifica los archivos.")
+    else:
+        st.error("Las columnas esperadas no están en TC2. Verifica los nombres de las columnas.")
